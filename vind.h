e88c6b2c6cbb75a67d864def3c98fcd91412c276
George <georgepombouridis05@gmail.com>
2026-04-20 18:54:26 +0200

    Update vind.h

--- vind.h
+++ vind.h
@@ -1,18 +1,23 @@
-//Separat fil där vindsensorn kopplas till temp sensor koden
 #ifndef __VIND_H
 #define __VIND_H
  
 #include "gd32vf103.h"
 #include <stdint.h>
 #include <math.h>
-
-/* AS5600 vindsensor I2C-adress */
-#define WIND_SENSOR_ADDR 0x36
  
-/* Läser vinkeln från sensorn och returnerar grader (0.0–360.0) */
-float wind_read_degrees(void);
+/* Flagga som sätts av timer-avbrottet */
+extern volatile uint8_t read_flag;
+ 
+// Konfigurerar timer för vindsensorn //
+void timer_config(uint32_t timer_periph, uint32_t time_interval_ms);
+ 
+/* Läser från ett I2C-register */
+void i2c_read_register(int32_t port, uint8_t addr, uint8_t reg, uint8_t size, uint8_t *data);
+ 
+/* Skriver till ett I2C-register */
+void i2c_write_register(int32_t port, uint8_t addr, uint8_t reg, uint8_t size, uint8_t *data);
  
 /* Beräknar medelvinkeln från en array av vinklar */
-float wind_get_average(float angles[], int count);
+float getAverageAngle(float angles[], int count);
  
 #endif
