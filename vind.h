26902bbd8e21bc749b62d4c88e39d5b7bef7b69f
tesun1337 <88622576+tesun1337@users.noreply.github.com>
2026-04-21 17:43:01 +0200

    ny vind.h

--- vind.h
+++ vind.h
@@ -1,23 +1,12 @@
 #ifndef __VIND_H
 #define __VIND_H
- 
-#include "gd32vf103.h"
+
 #include <stdint.h>
-#include <math.h>
- 
-/* Flagga som sätts av timer-avbrottet */
-extern volatile uint8_t read_flag;
- 
-// Konfigurerar timer för vindsensorn //
-void timer_config(uint32_t timer_periph, uint32_t time_interval_ms);
- 
-/* Läser från ett I2C-register */
-void i2c_read_register(int32_t port, uint8_t addr, uint8_t reg, uint8_t size, uint8_t *data);
- 
-/* Skriver till ett I2C-register */
-void i2c_write_register(int32_t port, uint8_t addr, uint8_t reg, uint8_t size, uint8_t *data);
- 
-/* Beräknar medelvinkeln från en array av vinklar */
+
+#define VIND_SENSOR_ADDR 0x36
+
+void vind_init_setup(void);
+int vind_read_raw(uint16_t *vind_raw);
+void vind_read_degrees(uint16_t vind_raw, int *vind_deg);
 float getAverageAngle(float angles[], int count);
- 
 #endif
