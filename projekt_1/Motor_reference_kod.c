242468713da07c655ee97703614c492586427624
maddibasaho <maddibasaho@gmail.com>
2026-05-12 11:06:31 +0200

    Enhance motor control and input initialization
    Refactor motor control logic and add input handling.

--- projekt_1/Motor_reference_kod.c
+++ projekt_1/Motor_reference_kod.c
@@ -7,16 +7,26 @@
 #define EI 1
 #define DI 0
 
+void rotera(int pinMal){
+	
+	int vinkel = 5;
+
+	do{
+		T1setPWMch1((30 / 100.0) * 16000);  // Den sätter in movement till ch1 alltså A1 och sätter ch2 alltså A2 till 0 så den snurrar åt en riktning
+		T1setPWMch2(0);
+		vinkel = gpio_input_bit_get(GPIOB,pinMal); //Tar in status från plattan som ska nuddas av metallstaven
+	} while (vinkel != 0); //checkar om den är förändrad dvs om pinnen är där 
+
+}
+
 
 int main(){
-	uint32_t vindData; //sparar vinddatan
-	int fPosition = 0;
-	int position = 0;
-	int done = 0;
-	int riktning = 0;
 	
-	
-	
+	int onskatLage; //ser vilken vinkel snökanonen vill vrida sig mot
+	uint32_t lage; //tittar checkar läget
+	int toggle = 0;//bestämmer hur själva motorn ska snurra
+	int grader = 0; //tar emot vinddatans tal
+	int bGrader = 0; //en checkup för om motorn behöver vrida på sig
 
 
 	/*användbara funktioner för motorn*/
@@ -36,66 +46,89 @@ int main(){
 
 	/*inputs*/
 
-	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_0);; // B0 är den pinnen som ska ta emot data från vindsensorn
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_1); //Dessa initerar bara pinben för olika riktningarna
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_2);
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_3);
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_5); 
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_7);
+	gpio_init(GPIOB, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, GPIO_PIN_8);
+
+	gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, GPIO_PIN_8); //möjligtvis för att ta emot vinddata behöver hjälp
 	/*outputs*/
 
 	T1powerUpInitPWM(0x6); //gör A1 och A2 på mcun är PWM outputsen som man ändra på styrkan 0%-100%. Det är outputsen till själva h-bryggan som ska driva motorn
-	//T1setPWMch1((movement / 100.0) * 16000);  // Den sätter in movement till ch1 alltså A1 och sätter ch2 alltså A2 till 0 så den snurrar åt en riktning
-	//T1setPWMch2(0);
+	
 
+	do{
+		T1setPWMch1((30 / 100.0) * 16000);  // Den sätter in movement till ch1 alltså A1 och sätter ch2 alltså A2 till 0 så den snurrar åt en riktning
+		T1setPWMch2(0);
+		lage = gpio_input_bit_get(GPIOB,1); //Tar in status från plattan som ska nuddas av metallstaven
+	} while (lage != 0); //checkar om den är förändrad dvs om pinnen är där 
+			
 	while(1){
 
-		vindData = gpio_input_bit_get(GPIOB,GPIO_PIN_0);
+		grader = 1/*Sätt att ta in vinddatans siffor*/;
 
-		if(vindData>0){
+		if(grader != bGrader){
 
-			if(position < vindData){
-				riktning = 0;
-			} else {
-				riktning = 1;
+			bGrader = grader;
+
+			//denna sektionen tar reda på vilket det onskadelaget är
+
+			if(grader>=0 && grader<45){
+				onskatLage = 1;
+			} 
+
+			if(grader>=45 && grader<90){
+				onskatLage = 2;
+			} 
+
+			if(grader>=90 && grader<135){
+				onskatLage = 3;
+			} 
+
+			if(grader>=135 && grader<180){
+				onskatLage = 4;
+			} 
+
+			if(grader>=180 && grader<225){
+				onskatLage = 5;
+			} 
+
+			if(grader>=225 && grader<270){
+				onskatLage = 6;
+			} 
+
+			if(grader>=270 && grader<315){
+				onskatLage = 7;
+			} 
+
+			if(grader>=315 && grader<360){
+				onskatLage = 8;
 			}
 
-			if(position == vindData){
-				T1setPWMch1(1);
-				T1setPWMch2(1);
-			} else {
-				
-				switch(riktning){
-				case 0:
-				if(position > (vindData/2)){
-					T1setPWMch1(10);
-					T1setPWMch2(0);
-					position = position + 1;
-				} else {
-					T1setPWMch1(30);
-					T1setPWMch2(0);
-					position = position + 1;
-				};break;
-
-				case 1:
-
-				if((position/2) > vindData){
-					T1setPWMch2(10);
-					T1setPWMch1(0);
-					position = position + -1;
-				} else {
-					T1setPWMch2(30);
-					T1setPWMch1(0);
-					position = position - 1;
-
-				} break;
+			if(grader == 360){
+				onskatLage = 1;
 			}
-		}
-	} else {
-		if(position != fPosition){
-			T1setPWMch2(30);
-			T1setPWMch1(0);
-			position=position-1;
-		} else {
-			T1setPWMch2(1);
-			T1setPWMch1(1);
+
+			//här är en switch för vilken platta den ska rotera till (siffrorna som skickas in är bara placeholders för nu)
+
+			switch(onskatLage){
+
+				case 1: rotera(1);break;
+				case 2: rotera(2);break;
+				case 3: rotera(3);break;
+				case 4: rotera(4);break;
+				case 5: rotera(5);break;
+				case 6: rotera(6);break;
+				case 7: rotera(7);break;
+				case 8: rotera(8);break;
+
+
+			}
 		}
 	}
-}
 }
 
