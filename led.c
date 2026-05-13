ee49a09e151a12c3b73869ffb152e674736aad24
kimornberg-sketch <kim.ornberg@gmail.com>
2026-05-13 12:39:37 +0200

    Change LED control to GPIOB from GPIOA

--- led.c
+++ led.c
@@ -3,16 +3,16 @@
 
 void led_init(void){
 
-    rcu_periph_clock_enable(RCU_GPIOA);
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_1);
+    rcu_periph_clock_enable(RCU_GPIOB);
+    gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_1);
 }
 
 void led_on(void)
 {
-    gpio_bit_set(GPIOA, GPIO_PIN_1);
+    gpio_bit_set(GPIOB, GPIO_PIN_1);
 }
 
 void led_off(void)
 {
-    gpio_bit_reset(GPIOA, GPIO_PIN_1);
+    gpio_bit_reset(GPIOB, GPIO_PIN_1);
 }
