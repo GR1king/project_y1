1cc4614876692dbee57700508fc38dcf97646836
maddibasaho <maddibasaho@gmail.com>
2026-05-29 12:15:04 +0200

    Update pwm.c

--- projekt_1/pwm.c
+++ projekt_1/pwm.c
@@ -58,6 +58,10 @@ void T1powerUpInitPWM(uint16_t ch){
     timer_enable(TIMER1);
 }
 
+void T1setPWMch3(int value){
+    timer_channel_output_pulse_value_config(TIMER1,TIMER_CH_3,value);
+}
+
 void T1setPWMch2(int value){
     timer_channel_output_pulse_value_config(TIMER1,TIMER_CH_2,value);
 }
